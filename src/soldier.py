8cfbba0e5e2b8953d6418c5652b591382a593363
tzirkus <zalmen5779@gmail.com>
2025-08-19 17:27:04 +0300

    soldier file

---
+++ src/soldier.py
@@ -0,0 +1,7 @@
+class Soldier:
+    def __init__(self, soldier_id, first_name, last_name, phone_number, rank):
+        self.id = soldier_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.rank = rank
